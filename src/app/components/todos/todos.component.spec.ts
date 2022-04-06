0044b72e952192320e0ecdd16d3e6dc957e4b650
Aschroft <chrisayrton@icloud.com>
2022-04-06 12:14:51 +0700

    Add unit tests for todo component

--- src/app/components/todos/todos.component.spec.ts
+++ src/app/components/todos/todos.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodosComponent } from './todos.component';
+import { TodoService } from '../../services/todo.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { waitForAsync } from '@angular/core/testing';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -8,7 +11,9 @@ describe('TodosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodosComponent ]
+      imports: [ HttpClientTestingModule ],
+      declarations: [ TodosComponent ],
+      providers: [ TodoService ]
     })
     .compileComponents();
   });
@@ -19,7 +24,27 @@ describe('TodosComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create todos component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('name should be empty string', () => {
+    expect(component.name).toBe('');
+  });
+
+  it('should be empty array', () => {
+    expect(component.todos.length).toEqual(0);
+    expect(component.todos.length).toHaveSize(0);
+    expect(component.editMode.length).toEqual(0);
+    expect(component.editMode.length).toHaveSize(0);
+  });
+
+  it('should return data from service function getTodos', waitForAsync(() => {
+    const result = [] as  any;
+    let mockTodoService: jasmine.SpyObj<TodoService>;
+    mockTodoService = jasmine.createSpyObj('TodoService', ['getTodos']);
+    mockTodoService.getTodos.and.returnValue(result);
+    fixture.detectChanges();
+    expect(component.todos).toEqual(result);
+  }))
 });
